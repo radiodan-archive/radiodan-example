307177f61b5d844c4c2598c89fdbf47a33f1c0ec
Dan Nuttall <pixelblend@gmail.com>
2013-06-30 03:10:13 +0100

    Show player state in realtime

--- lib/web_server.rb
+++ lib/web_server.rb
@@ -1,12 +1,16 @@
+require 'json'
 class WebServer < Sinatra::Base
   register Sinatra::Async
+  
   def initialize(player)
     @player = player
     super()
   end
 
-  get '/' do
-    '<h1>Radiodan</h1>'
+  aget '/' do
+    EM::Synchrony.next_tick do
+      body { "<h1>Radiodan</h1><p>#{CGI.escapeHTML(@player.state.inspect)}</p>" }
+    end
   end
 
   aget '/panic' do
