14da47ef28f402a1b39d53d98725045531b7a472
Dan Nuttall <pixelblend@gmail.com>
2013-08-27 16:39:15 +0100

    Refactor web app to match gem update

---
+++ lib/web_app.rb
@@ -0,0 +1,26 @@
+require 'radiodan/sinatra'
+
+class WebApp < Radiodan::Sinatra
+  get '/' do
+    "<h1>Radiodan</h1><p>#{CGI.escapeHTML(player.state.inspect)}</p>"
+  end
+
+  get '/search/:query' do
+    query = params[:query]
+    
+    search = player.search(query)
+    player.playlist = Radiodan::Playlist.new tracks: search
+    "<h1>Radiodan</h1><p>#{CGI.escapeHTML(search.inspect)}</p>"
+  end
+  
+  get '/play.mp3' do
+    search = player.search(:filename => 'Ween/Chocolate and Cheese/10 Voodoo Lady.mp3')
+    player.playlist = Radiodan::Playlist.new mode: :resume, tracks: search  
+    "<h1>Radiodan</h1><p>#{CGI.escapeHTML(player.state.inspect)}</p>"
+  end
+
+  get '/panic' do
+    player.trigger_event :panic
+    "Panic!"
+  end
+end
