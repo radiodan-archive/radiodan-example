c03e184931b1e2bdb1e550a1dc5c3c84ff18bf20
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2013-11-20 19:24:23 +0000

    Save/restore current station/volume from preferences file

---
+++ lib/preference.rb
@@ -0,0 +1,39 @@
+require 'yaml'
+
+class Preference
+  include Radiodan::Logging
+
+  def initialize(*config)
+    @options  = config.shift
+    @path     = @options[:file_path]
+    @defaults = @options[:defaults]
+    @prefs    = {}
+  end
+
+  def call(player)
+    player.register_event :playlist do |playlist|
+      @current_station = playlist.tracks.first[:id]
+      @prefs[:station_id] = @current_station
+      save!
+    end
+
+    player.register_event :change_volume do |volume|
+      @prefs[:volume] = volume
+      save!
+    end
+  end
+
+  def method_missing(method, *args, &block)
+    load!
+    @prefs[method] if @prefs.has_key?(method)
+  end
+
+  private
+  def save!
+    File.open(@path, 'w') { |f| YAML.dump(@prefs, f) }
+  end
+
+  def load!
+    @prefs = YAML.load_file(@path) || {}
+  end
+end
