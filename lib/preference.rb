65299d26c6db70df3ccfab1efea95b5335419437
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2013-11-25 16:12:48 +0000

    Move preference retrieval to Preferences class

--- lib/preference.rb
+++ lib/preference.rb
@@ -8,11 +8,24 @@ class Preference
     @path     = @options[:file_path]
     @defaults = @options[:defaults]
     @playlist = @options[:playlist]
+    @stations = @options[:stations]
     @prefs    = {}
   end
 
   def call(player)
 
+    # Setup preferences for initial station
+    # If no initial station saved, choose a random one
+    initial_station = @stations[station_id] || @stations[@stations.keys.sample]
+
+    # If no initial volume, choose 50%
+    initial_volume  = volume || 50
+
+    # Set initial settings
+    @playlist.tracks = initial_station.tracks
+    @playlist.volume = initial_volume
+
+    # Set playlist on the player
     player.playlist = @playlist
 
     player.register_event :playlist do |playlist|
